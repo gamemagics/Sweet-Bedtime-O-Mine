118e4cf2f6a98b3acc9bcba51e76eaa5a2550e0e
NeilKleistGao <neilkleistgao@bupt.edu.cn>
2022-04-03 15:21:53 +0800

    Add electric drill agent

---
+++ Assets/Scripts/Monsters/ElectricDrill/ElectricDrillCollider.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ElectricDrillCollider : MonoBehaviour {
+    void OnTriggerEnter2D(Collider2D collider) {
+        transform.parent.gameObject.GetComponent<ElectricDrillAgent>().Stop();
+    }
+}
