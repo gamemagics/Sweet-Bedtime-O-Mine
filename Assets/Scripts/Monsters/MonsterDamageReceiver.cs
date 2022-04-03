48abf9863dd39d18eea887bc7b56fe92c7c11ad6
NeilKleistGao <neilkleistgao@bupt.edu.cn>
2022-04-03 09:43:04 +0800

    Add clock walk anim

--- Assets/Scripts/Monsters/MonsterDamageReceiver.cs
+++ Assets/Scripts/Monsters/MonsterDamageReceiver.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class MonsterDamageReceiver : MonoBehaviour {
-    [SerializeField] private int HP;
+    [SerializeField] private int HP = 1;
 
     void OnTriggerEnter2D(Collider2D collider) {
         if (collider.tag == "PlayerAttack") {
