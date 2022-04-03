a69d383f47b0bcce5de8480916d5fd8569101864
NeilKleistGao <neilkleistgao@bupt.edu.cn>
2022-04-03 12:31:29 +0800

    Add digital clock anim

---
+++ Assets/Scripts/Monsters/DigitalClock/DigitalClockProxy.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DigitalClockProxy : MonoBehaviour {
+    public enum DigitalClockColor {
+        RED = 0, YELLOW = 1, BLUE = 2
+    }
+
+    [SerializeField] private GameObject[] digitalClockPrefabs = null;
+
+    private List<GameObject> clocks = new List<GameObject>();
+    private List<int> order = new List<int>();
+
+    public void InitByColor(DigitalClockColor color, Vector2[] positions) {
+        // TODO:
+    }
+}
