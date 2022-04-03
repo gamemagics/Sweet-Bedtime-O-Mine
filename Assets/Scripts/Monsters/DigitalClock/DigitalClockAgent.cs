a1296f970279345dbb8b069ca7910a7b24e903c4
NeilKleistGao <neilkleistgao@bupt.edu.cn>
2022-04-03 13:45:46 +0800

    Add digital clock agent

---
+++ Assets/Scripts/Monsters/DigitalClock/DigitalClockAgent.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+using UnityEngine.Events;
+
+public class DigitalClockAgent : MonoBehaviour {
+    private enum DigitalClockState {
+        RUN, SHUT_DOWN, RESTART
+    }
+
+    private DigitalClockState state = DigitalClockState.RUN;
+    private Animator animator;
+
+    private GameObject player = null;
+
+    public int index = 0;
+
+    public UnityEvent<int> OnShutDown;
+
+    private static readonly float SPEED = 0.5f;
+
+    [SerializeField] private int HP = 1;
+
+    void Awake() {
+        player = GameObject.FindGameObjectWithTag("Player");
+        animator = GetComponent<Animator>();
+    }
+
+    void Update() {
+        switch (state) {
+            case DigitalClockState.RUN:
+                UpdateRun();
+                break;
+            case DigitalClockState.SHUT_DOWN:
+                break;
+            case DigitalClockState.RESTART:
+                UpdateRestart();
+                break;
+        }
+    }
+
+    void UpdateRun() {
+        Vector2 direction = player.transform.position - transform.position;
+        direction.Normalize();
+        transform.Translate(-direction * SPEED * Time.deltaTime);
+
+        if (HP <= 0) {
+            animator.SetBool("Shut", true);
+            animator.ResetTrigger("Restart");
+            state = DigitalClockState.SHUT_DOWN;
+        }
+    }
+
+    void UpdateRestart() {
+        AnimatorStateInfo info = animator.GetCurrentAnimatorStateInfo(0);
+        if (info.normalizedTime >= 1.0f && info.IsName("Base Layer.Reset")) {
+            animator.SetTrigger("Restart");
+            state = DigitalClockState.RUN;
+        }
+    }
+
+    void OnCollisionEnter2D(Collision2D collision) {
+        if (collision.gameObject.tag == "") {
+            // TODO:
+        }
+    }
+
+    public void Reset() {
+        animator.SetBool("Shut", false);
+        state = DigitalClockState.RESTART;
+    }
+}
