3d0f96d41df68e6c44128fe461a5e1eba18a42b5
NeilKleistGao <neilkleistgao@bupt.edu.cn>
2022-04-04 08:37:58 +0800

    Add sun anim

---
+++ Assets/Scripts/Boss/SunAgent.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SunAgent : MonoBehaviour {
+    private GameObject player = null;
+
+    void Awake() {
+        player = GameObject.FindGameObjectWithTag("Player");
+    }
+
+    void Update() {
+        // TODO:
+    }
+}
