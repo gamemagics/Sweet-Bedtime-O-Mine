0de153cf08b779671cccf43565b08d0c3ee40911
NeilKleistGao <neilkleistgao@bupt.edu.cn>
2022-04-04 19:12:54 +0800

    Add pause

---
+++ Assets/Scripts/UI/PauseUI.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseUI : MonoBehaviour {
+    [SerializeField] private GameObject BB;
+    public void Pause() {
+        Time.timeScale = 0;
+        BB.SetActive(true);
+    }
+
+    public void Resume() {
+        Time.timeScale = 1;
+        BB.SetActive(false);
+    }
+}
