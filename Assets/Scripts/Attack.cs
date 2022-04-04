8f12be09085bf69226b0d6366cd1906fef912b3c
kami <815918091@qq.com>
2022-04-04 09:51:24 +0800

    .history folder ignored

---
+++ Assets/Scripts/Attack.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Attack : MonoBehaviour
+{
+    public GameObject projectilePrefab;
+    public float damage;
+    public float projectileForce;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.J))
+        {
+            GameObject projectile = Instantiate(projectilePrefab, transform.position, Quaternion.identity);
+            Vector2 direction = transform.position.normalized;
+            projectile.GetComponent<Rigidbody2D>().velocity = direction * projectileForce;
+            //projectile.GetComponent<>
+        }
+    }
+}
