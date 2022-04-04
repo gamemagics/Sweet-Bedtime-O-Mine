7c519dce5213fecdbd65766ba23fb73b3c551b79
NeilKleistGao <neilkleistgao@bupt.edu.cn>
2022-04-04 09:21:43 +0800

    Add fire ball

---
+++ Assets/Scripts/Boss/FireBall.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FireBall : MonoBehaviour {
+    private Animator animator = null;
+    private SpriteRenderer spriteRenderer = null;
+
+    private FireBallDamage damage = null;
+
+    [SerializeField] private GameObject fall = null;
+    [SerializeField] private GameObject crash = null;
+
+    void Awake() {
+        animator = GetComponent<Animator>();
+        spriteRenderer = fall.GetComponent<SpriteRenderer>();
+        damage = crash.GetComponent<FireBallDamage>();
+    }
+
+    void Update() {
+        AnimatorStateInfo info = animator.GetCurrentAnimatorStateInfo(0);
+        if (info.normalizedTime >= 1.0f && info.IsName("Base Layer.Crash")) {
+            DestroyImmediate(gameObject);
+        }
+        else if (info.normalizedTime >= 1.0f && info.IsName("Base Layer.Fall")) {
+            animator.SetTrigger("Hit");
+            spriteRenderer.color = new Color(0, 0, 0, 0);
+            damage.Damage();
+        }
+    }
+}
--- Assets/Scripts/Boss/SunAgent.cs
+++ Assets/Scripts/Boss/SunAgent.cs
@@ -3,13 +3,40 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class SunAgent : MonoBehaviour {
+    private Animator animator = null;
     private GameObject player = null;
+    private static readonly float INTERVAL = 1.0f;
+    private float timer = 0.0f;
+
+    [SerializeField] private GameObject fireBallPrefab = null;
+    [SerializeField] private GameObject[] flamePrefab = null;
 
     void Awake() {
+        animator = GetComponent<Animator>();
         player = GameObject.FindGameObjectWithTag("Player");
     }
 
     void Update() {
-        // TODO:
+        if (timer < INTERVAL) {
+            timer += Time.deltaTime;
+        }
+        else {
+            timer -= INTERVAL;
+            AnimatorStateInfo info = animator.GetCurrentAnimatorStateInfo(0);
+            if (info.IsName("Base Layer.Attack1")) {
+                AttackFireBall();
+            }
+            else {
+                AttackFlame();
+            }
+        }
+    }
+
+    void AttackFireBall() {
+
+    }
+
+    void AttackFlame() {
+
     }
 }
